a609ce5204f8bd43bf10400abc241a296c265944
Alephtrade <123379384+Alephtrade@users.noreply.github.com>
2023-08-15 08:39:34 +0300

    authchecker

---
+++ app/Http/Controllers/Auth/AuthChecker.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Hash;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Session;
+use Validator;
+
+class AuthChecker extends Controller
+{
+    public function action(Request $request)
+    {
+        $response["is_auth"] = false;
+        $response["user"] = false;
+        $code = 403;
+        if (Auth::check()) {
+            $response["is_auth"] = True;
+            $response["user"] = Auth::user();
+            $code = 200;
+        }
+        return response()->json($response, $code);
+    }
+}
