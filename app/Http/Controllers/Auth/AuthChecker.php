6f678bc40ca680c82a97f6790cd83cbd843b8e94
it@alephtrade.com <it@alephtrade.com>
2023-09-09 11:15:18 +0300

    auth repaire need to must have start with artisan serve

--- app/Http/Controllers/Auth/AuthChecker.php
+++ app/Http/Controllers/Auth/AuthChecker.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Hash;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Session;
 use Validator;
 
