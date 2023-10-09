4789c5694bb275a6928d2a7ab75482174509197c
it@alephtrade.com <it@alephtrade.com>
2023-10-09 22:44:23 +0300

    artisan && report

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,23 +2,39 @@
 
 namespace App\Console;
 
+use App\Jobs\SendExcelController;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
     /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        \App\Console\Commands\SendExcel::class
+    ];
+
+    /**
      * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->job(new SendExcelController)->dailyAt('13:00');
+        // $schedule->command('command:SendExcel')->everyFifteenSeconds();
     }
 
     /**
      * Register the commands for the application.
+     *
+     * @return void
      */
-    protected function commands(): void
+    protected function commands()
     {
         $this->load(__DIR__ . '/Commands');
 
---
+++ resources/views/email.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Niktea</title>
+</head>
+<body style="        background: #fafafa url(https://jackrugile.com/images/misc/noise-diagonal.png);
+        color: #444;
+        font: 100%/30px 'Helvetica Neue', helvetica, arial, sans-serif;
+        text-shadow: 0 1px 0 #fff;">
+<h1>Добрый день, данные по зарегистрированным пользователям, по датам</h1>
+<table style='        background: #f5f5f5;
+        border-collapse: separate;
+        box-shadow: inset 0 1px 0 #fff;
+        font-size: 12px;
+        line-height: 24px;
+        margin: 30px auto;
+        text-align: left;
+        width: 800px;'>
+    <tr>
+        <th>Дата</th>
+        <th>Количество зарег. пользователей</th>
+
+    </tr>
+@foreach ($result as $date => $user_count)
+
+        <tr>
+            <td
+                style="border-right: 1px solid #fff;
+        border-left: 1px solid #e8e8e8;
+        border-top: 1px solid #fff;
+        border-bottom: 1px solid #e8e8e8;
+        padding: 10px 15px;
+        position: relative;
+        transition: all 300ms;">
+                {{ $date }}
+            </td>
+            <td
+                style="border-right: 1px solid #fff;
+        border-left: 1px solid #e8e8e8;
+        border-top: 1px solid #fff;
+        border-bottom: 1px solid #e8e8e8;
+        padding: 10px 15px;
+        position: relative;
+        transition: all 300ms;">{{ $user_count }}</td>
+
+@endforeach
+
+</table >
+</body>
+</html>
